d299bf5daa36ce1f6b037c1204043c79f613d4b0
Jakub Więckowski <kubaw1@onet.eu>
2024-02-13 18:00:59 +0100

    add criteria range logic

--- pysenscraft/criteria/range.py
+++ pysenscraft/criteria/range.py
@@ -6,74 +6,77 @@ from itertools import product
 def range_modification(weights: np.ndarray, range_values: np.ndarray, indexes: None | np.ndarray = None, step: float = 0.01):
     """
     Modify a set of criteria weights based on specified percentage changes, directions, and indexes.
+
     Parameters
     ----------
     weights : ndarray
         1D array representing the initial criteria weights. Should sum up to 1.
     range_values : ndarray
-        Percentage changes to be applied to the criteria weights. 
-        If ndarray, it specifies the percentage change for each criterion individually.
+        Range of values for each criterion specifying the allowed changes.
+        Should be given as a two-dimensional array where each row represents a criterion, 
+        and the columns represent the lower and upper bounds of the allowed range.
     indexes : None | ndarray, optional, default=None
         Indexes of the criteria to be modified. If None, all criteria are considered subsequently.
         If ndarray, it specifies the indexes or combinations of indexes for the criteria to be modified.
     step : float, optional, default=0.01
         Step size for the percentage change.
+
     Returns
     -------
-    List[Tuple[int, ndarray, ndarray]]
-        A list of tuples containing information about the modified criteria index, percentage change,
-        and the resulting criteria weights.
+    List[Tuple[int, Union[float, Tuple[float, ...]], ndarray]]
+        A list of tuples containing information about the modified criteria index, 
+        percentage change, and the resulting criteria weights.
+
     ## Examples
     --------
     ### Example 1: Modify weights with a single percentage change
     >>> weights = np.array([0.3, 0.3, 0.4])
-    >>> percentage = 5
-    >>> results = percentage_modification(weights, percentage)
+    >>> range_values = np.array([[0.25, 0.3], [0.3, 0.35], [0.37, 0.43]])
+    >>> results = range_modification(weights, range_values)
     >>> for r in results:
     ...     print(r)
-    # Example 2: Modify weights with percentages, specific indexes, and step size
+
+    ### Example 2: Modify weights with range values, specific indexes, and step size
     >>> weights = np.array([0.3, 0.3, 0.4])
-    >>> percentages = np.array([5, 5, 5])
     >>> indexes = np.array([[0, 1], 2], dtype='object')
-    >>> results = percentage_modification(weights, percentages, indexes=indexes)
+    >>> range_values = np.array([[0.25, 0.3], [0.3, 0.35], [0.37, 0.43]])
+    >>> results = range_modification(weights, range_values, indexes=indexes)
     >>> for r in results:
     ...     print(r)
-    ### Example 3: Modify weights with percentages and specific direction for each criterion
+
+    ### Example 3: Modify weights with range values and individual step sizes
     >>> weights = np.array([0.3, 0.3, 0.4])
-    >>> percentages = np.array([6, 4, 5])
-    >>> direction = np.array([-1, 1, -1])
-    >>> results = percentage_modification(weights, percentages, direction=direction)
-    >>> for r in results:
-    ...     print(r)
-    ### Example 4: Modify weights with percentages, specific indexes, and individual step sizes
-    >>> weights = np.array([0.3, 0.3, 0.4])
-    >>> percentages = np.array([6, 4, 8])
-    >>> indexes = np.array([0, 2])
-    >>> step = 2
-    >>> results = percentage_modification(weights, percentages, indexes=indexes, step=step)
+    >>> range_values = np.array([[0.25, 0.3], [0.3, 0.35], [0.37, 0.43]])
+    >>> step = 0.02
+    >>> results = range_modification(weights, range_values, step=step)
     >>> for r in results:
     ...     print(r)
     """
 
-    def modify_weights(weights, crit_idx, diff):
+    def modify_weights(weights, crit_idx, change):
         new_weights = weights.copy()
 
         modified_criteria = 1
         if isinstance(crit_idx, (int, np.integer)):
-            new_weights[crit_idx] = weights[crit_idx] + diff
+            new_weights[crit_idx] = change
         else:
-            modified_criteria = len(crit_idx)
-            new_weights[crit_idx] = weights[crit_idx] + diff
+            if np.sum(change) >= 1:
+                return None
 
-        equal_diff = np.sum(diff) / (weights.shape[0] - modified_criteria)
+            modified_criteria = len(crit_idx)
+            new_weights[crit_idx] = change
+
+        new_sum = np.sum(new_weights)
+        adjust_direction = -1 if new_sum > 1 else 1
+        equal_diff = np.abs(1 - new_sum) / (weights.shape[0] - modified_criteria)
         # adjust weights to sum up to 1
         for idx, w in enumerate(weights):
             if isinstance(crit_idx, (int, np.integer)):
                 if crit_idx != idx:
-                    new_weights[idx] = w + equal_diff
+                    new_weights[idx] = w + equal_diff * adjust_direction
             else:
                 if idx not in crit_idx:
-                    new_weights[idx] = w + equal_diff
+                    new_weights[idx] = w + equal_diff * adjust_direction
 
         return new_weights / np.sum(new_weights)
 
@@ -108,7 +111,8 @@ def range_modification(weights: np.ndarray, range_values: np.ndarray, indexes: N
     results = []
 
     # generation of vector with subsequent values of weights for criteria
-    range_changes = np.array([np.linspace(range_values[i][0], range_values[i][1], int((range_values[i][1] - range_values[i][0])/step)+1) for i in range(weights.shape[0])], dtype='object')
+    range_changes = np.array([np.arange(range_values[i][0], range_values[i][1]+step, step) for i in range(weights.shape[0])], dtype='object')
+    range_changes = np.array([[val for val in rc if val >= range_values[idx][0] and val <= range_values[idx][1]] for idx, rc in enumerate(range_changes)])
 
     # criteria indexes to modify weights values
     indexes_values = None
@@ -116,33 +120,17 @@ def range_modification(weights: np.ndarray, range_values: np.ndarray, indexes: N
         indexes_values = np.arange(0, weights.shape[0], dtype=int)
     else:
         indexes_values = indexes
-    
-    print(range_changes)
-    print(indexes_values)
 
-    # for crit_idx in indexes_values:
-    #     if isinstance(crit_idx, (int, np.integer)):
-    #         changes = percentages_changes[crit_idx]
-    #     else:
-    #         changes = list(product(*percentages_changes[crit_idx]))
+    for crit_idx in indexes_values:
+        if isinstance(crit_idx, (int, np.integer)):
+            changes = range_changes[crit_idx]
+        else:
+            changes = list(product(*range_changes[crit_idx]))
 
-    #     for change in changes:
-    #         diff = weights[crit_idx] * change
-
-    #         for val in direction_values[crit_idx]:
-    #             if isinstance(val, (int, np.integer)):
-    #                 new_weights = modify_weights(weights, crit_idx, diff, val)
-    #                 results.append((crit_idx, change * val, new_weights))
-    #             else:
-    #                 for v in [-1, 1]:
-    #                     new_weights = modify_weights(weights, crit_idx, diff, v)
-    #                     results.append((crit_idx, np.asarray(change) * v, new_weights))
+        for change in changes:
+            change_val = np.round(change, 6) if isinstance(change, np.float) else tuple(np.round(change, 6).tolist())
+            new_weights = modify_weights(weights, crit_idx, change)
+            if new_weights is not None:
+                results.append((crit_idx, change_val, new_weights))
 
     return results
-
-# Example 1
-weights = np.array([0.3, 0.3, 0.4])
-range_values = np.array([[0.25, 0.3], [0.3, 0.35], [0.37, 0.43]])
-results = range_modification(weights, range_values)
-for r in results:
-    print(r)
